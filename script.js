e83132deb5fb5cd075f69ff85e728420ba930837
alanmatheusc <alanmatheuscardosodoamaral@gmail.com>
2024-07-19 18:56:16 -0500

    todo-list

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
- let userTimeWheaterData;
- 
  (function getUserLocation(){
      navigator.geolocation.getCurrentPosition(position =>{
-        getTimeWheaterByLocation(position.coords)
+        getTimeWheaterByLocation(position.coords);
     })
 })();
 
+let userTimeWheaterData;
+
+let userToDoList = []
 
 async function getTimeWheaterByLocation(userPosition){
 try{
@@ -13,15 +14,59 @@ try{
     await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=72857d324dc5a0d7891a61a5f0bffc48`)
     .then(resp => resp.json())
     .then(data =>{
-        userTimeWheaterData = data
-        console.log(data)
+        userTimeWheaterData = data;
+        addWeatherDataToUserScreen(userTimeWheaterData);
     })
 }catch(error){
-    console.log("User denied Location permissions")
+    console.log("User denied Location permissions");
 }
 }
 
+function addWeatherDataToUserScreen(wheaterData){
+    if(wheaterData === null){
+        return;
+    }
+}
 
 function convertKelvinToCelsius(kelvin){
-    return kelvin - 273,15
+    return kelvin - 273,15;
+}
+
+
+function getToDoList(){
+    let input = document.getElementById('toDo').value;
+    userToDoList.push({
+        task:input
+    })
+
+    addToDoListAtLocalStorage(userToDoList);
+    addListAtUserScreen()
+    }
+
+function addToDoListAtLocalStorage(toDoList){
+    console.log(toDoList)
+    if(localStorage.getItem('toDo') === null){
+        localStorage.setItem("toDo", toDoList);
+    }
+    localStorage.setItem("toDo",JSON.stringify(toDoList));
+}
+
+
+function addListAtUserScreen(){
+    let toDoListLocalStorage = JSON.parse(localStorage.getItem("toDo"));
+    const tabela = document.getElementById('tabela');
+    const tbody = tabela.querySelector('tbody');
+    const tr = document.createElement('tr');
+    const task = document.createElement('td');
+    const deleteButton = document.createElement('td');
+    const button = document.createElement('button')
+    button.setAttribute('type','button')
+    button.classList.add( 'btn-close', "btn-close-white");
+    deleteButton.appendChild(button)
+        for(let i = 0; i<toDoListLocalStorage.length; i++){
+            task.textContent = toDoListLocalStorage[i].task
+            tr.appendChild(task)
+            tr.appendChild(deleteButton)
+             tbody.appendChild(tr);
+        }
 }
